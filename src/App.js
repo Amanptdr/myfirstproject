fdfe1dc5490cf39c36dc0a7abf69f0ce7012de44
amanptdr <amanpatidar.synergytop@gmai.com>
2022-10-22 11:47:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const data = [{"name":"shivam Patidar","age":20,"class":"12th"},
+                {"name":"Arpit Patidar","age":28,"class":"10th"},
+                {"name":"Ajay Patidar","age":22,"class":"11th"}];
+  const functioncall =()=>{
+    console.log("shmghlfdblgnfbj");
+  }
+  return (
+    <div className="App">
+      TESTING
+      {data.map((item)=>
+      <div>{item.name}</div>      
+      )}
+      <button onClick={(e)=>functioncall(e)}>function</button>
+     </div>
+  )
+
+}
+export default App;
